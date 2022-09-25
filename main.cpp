6df1cc32e4697c7dc1acf23b680d9e22e0225413
Leokiko <o2239@qq.com>
2022-09-25 15:41:03 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,6 +4,24 @@
 using namespace std;
 #define MAX 1000
 
+//通讯录DB，包含五个成员变量	:包括姓名、性别、年龄、电话、住址
+struct Mem
+{
+	string name;
+	bool sex;
+	int age;
+	string tel;
+	string add;
+};
+
+//通讯录结构体设计，包括保存的联系人结构体、记录个数
+struct Book
+{
+	struct Mem arr[MAX];
+	int size;
+};
+
+
 //菜单界面
 void showMenu() {
 	cout << "*************************" << endl;
@@ -18,20 +36,23 @@ void showMenu() {
 }
 
 //添加联系人
-void add() {
-	int age;
-	string name, tel, add;
-	bool sex;
+void add(Book* a) {
+	Mem temp;
 	cout << "请输入姓名" << endl;
-	cin >> name;
+	cin >> temp.name;
 	cout << "请输入性别" << endl;
-	cin >> sex;
+	cin >> temp.sex;
 	cout << "请输入年龄" << endl;
-	cin >> age;
+	cin >> temp.age;
 	cout << "请输入电话" << endl;
-	cin >> tel;
+	cin >> temp.tel;
 	cout << "请输入住址" << endl;
-	cin >> add;
+	cin >> temp.add;
+	a->arr[a->size] = temp;
+	a->size++;
+	//也可以使用解引用来使用结构体数据
+	//(*a).arr[(*a).size] = temp;
+	//(*a).size++;
 }
 
 //显示联系人
@@ -50,30 +71,17 @@ void change() {}
 void empty() {}
 
 //退出系统
-void exit() {}
+void exit() {
+	cout << "系统已退出，欢迎下次使用！" << endl;
+	system("pause");
+}
 
-int mainn() {
-
-	//联系人结构体设计，包括姓名、性别、年龄、电话、住址
-	struct Mem
-	{
-		string name;
-		bool sex;
-		int age;
-		string tel;
-		string add;
-	};
-
-	//通讯录结构体设计，包括保存的联系人结构体、记录个数
-	struct Book
-	{
-		struct Mem arr[MAX];
-		int size;
-	};
+int main() {
 
 	//初始化通讯录
-	//Book db;
-	//db.size = 0;
+	//size用来控制Mem数量，0代表为空，有值则代表Mem个数
+	Book db;
+	db.size = 0;
 
 	//显示欢迎菜单
 	showMenu();
@@ -84,11 +92,9 @@ int mainn() {
 	{
 		switch (select)
 		{
-		case 0:
-			cout << "系统已退出，欢迎下次使用！" << endl;
-			system("pause");
+		case 0:exit();
 			return 0;
-		case 1:add();
+		case 1:add(&db);
 			break;
 		case 2:show();
 			break;
